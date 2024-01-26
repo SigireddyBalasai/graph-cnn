b8fa588f7b8360bcb4d5dcb30069355791159eb6
Your Name <you@example.com>
2024-01-26 09:26:26 +0530

    Add auxiliary layer and set seed for reproducibility

--- graph_cnn/model/model.py
+++ graph_cnn/model/model.py
@@ -1,4 +1,35 @@
-def create_model(graph, input_shape=(224, 224, 3), num_classes=100, use_mean=True, include_aux=True):
+import tensorflow as tf
+import numpy as np
+import networkx as nx
+import random
+from matplotlib import pyplot as plt
+from graph_cnn.graph.generate import create_final_graph
+from statistics import mean
+
+def set_seed(seed):
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    random.seed(seed)
+
+class AuxLayer(tf.keras.layers.Layer):
+    def __init__(self, num_classes):
+        super(AuxLayer, self).__init__()
+        self.num_classes = num_classes
+        self.layers_list = []
+
+    def build(self, input_shape):
+        current_shape = input_shape
+        print(current_shape)
+        self.layers_list.append(tf.keras.layers.Flatten())
+        self.layers_list.append(tf.keras.layers.Dropout(0.2))
+        self.layers_list.append(tf.keras.layers.Dense(units=self.num_classes, activation='softmax'))
+    def call(self, inputs):
+        for layer in self.layers_list:
+            inputs = layer(inputs)
+        return inputs
+
+
+def create_model(graph, input_shape=(224, 224, 3), num_classes=100, use_mean=True,include_aux=True):
     nodes = {}
     input_layer = tf.keras.layers.Input(shape=input_shape)
     for node in graph.nodes():
@@ -46,11 +77,6 @@ def create_model(graph, input_shape=(224, 224, 3), num_classes=100, use_mean=Tru
                 nodes[node] = concat
         dropout_prob = random.uniform(0.2, 1)
         nodes[node] = tf.keras.layers.Dropout(dropout_prob)(nodes[node])
-        # Randomly add activation layer and local connected 2D
-        if random.choice([True, False]):
-            nodes[node] = tf.keras.layers.Activation(graph.nodes[node]['activation'])(nodes[node])
-        if random.choice([True, False]):
-            nodes[node] = tf.keras.layers.Conv2D(filters=nodes[node].shape[-1], kernel_size=(3, 3), padding='same')(nodes[node])
     node_s = [nodes[node] for node in graph.nodes() if graph.out_degree(node) == 0]
     if use_mean:
         req_shape = max([x.shape[-1] for x in node_s])
@@ -74,3 +100,13 @@ def create_model(graph, input_shape=(224, 224, 3), num_classes=100, use_mean=Tru
     else:
         model = tf.keras.Model(inputs=input_layer,outputs=output_concat)
     return model
+
+
+if __name__ == '__main__':
+    set_seed(42)
+    graph = create_final_graph(7, 0.4)
+    nx.draw(graph, with_labels=True)
+    plt.show()
+    model = create_model(graph)
+    tf.keras.utils.plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True, expand_nested=True)
+    model.summary()
