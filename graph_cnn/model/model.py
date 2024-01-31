dd9de9ce4d76049897071ba51ef155d886a450a3
Your Name <you@example.com>
2024-01-31 14:20:04 +0530

    Update model creation and training parameters

--- graph_cnn/model/model.py
+++ graph_cnn/model/model.py
@@ -7,7 +7,7 @@ import networkx as nx
 from matplotlib import pyplot as plt
 from graph_cnn.graph.generate import create_final_graph
 from keras.layers import Conv2D,GlobalAveragePooling2D,Flatten,Dense,Input,Concatenate,Dropout,BatchNormalization,Activation,AveragePooling2D,LocallyConnected2D,MaxPooling2D,Add
-
+from keras.optimizers import SGD
 
 def set_seed(seed):
     """
@@ -34,7 +34,7 @@ def aux_layer(x, num_classes):
     """
     global_avg_pool = GlobalAveragePooling2D()(x)
     flat = Flatten()(global_avg_pool)
-    fc = Dense(units=num_classes, activation="softmax")(flat)
+    fc = Dense(units=num_classes, activation="sigmoid")(flat)
     return fc
 
 
@@ -85,10 +85,10 @@ def create_model(
     nodes = {}
     input_layer = Input(shape=input_shape)
     for node_ in graph.nodes():
+        print(node_)
         predecessors = list(graph.predecessors(node_))
         if not predecessors:
             filters = 32
-            print(graph.nodes[node_])
             conv1 = Conv2D(
                 filters=filters,
                 kernel_size=graph.nodes[node_]["kernel_size"],
@@ -100,67 +100,83 @@ def create_model(
             current = load_layer(node_d)(input_layer)
             conv_shape = conv1.shape[1]
             current_shape = current.shape[1]
-            print(conv_shape,current_shape)
             kernel_size = abs(current_shape - conv_shape) + 1
             conv_dim = conv1.shape[-1]
             current_dim = current.shape[-1]
-            filters = max(conv_dim, current_dim)
-            print(filters, current_dim, conv_dim, kernel_size)
+            filters = max(conv_dim, current_dim) * 2
             node_d["kernel_size"] = (kernel_size, kernel_size)
             node_d["filters"] = filters
             node_d['layer_type'] = 'Convolution'
             if current_dim != filters:
                 current = load_layer(node_d)(current)
-            elif conv_dim != filters:
+            if conv_dim != filters:
                 conv1 = load_layer(node_d)(conv1)
-            print(conv1.shape, current.shape)
             concatenate = Add()([conv1, current])
             normalized = BatchNormalization()(concatenate)
             pool = MaxPooling2D()(normalized)
             activaton = Activation(graph.nodes[node_]["activation"])(pool)
-            drop = Dropout(0.9)(activaton)
+            drop = Dropout(0.2)(activaton)
             nodes[node_] = drop
-            print('inputs_built')
+            print(drop)
         else:
             if len(predecessors) > 1:
                 if use_mean:
-                    req_shape = int(mean([nodes[x].shape[-1] for x in predecessors]))
+                    req_shape = int(mean([nodes[x].shape[-1] for x in predecessors]))*2
                 else:
-                    req_shape = max([nodes[x].shape[-1] for x in predecessors])
-                req_dimension = max([nodes[x].shape[1] for x in predecessors])
+                    req_shape = max([nodes[x].shape[-1] for x in predecessors])*2
+                req_dimension = min([nodes[x].shape[1] for x in predecessors])
+                node_list = []
                 for predecessor in predecessors: 
                     kernel_size = nodes[predecessor].shape[1] - req_dimension + 1
                     if (
-                        (
-                        nodes[predecessor].shape[-1] != req_shape
-                        or nodes[predecessor].shape[1] != req_dimension
-                        ) and kernel_size >=1
+                        nodes[predecessor].shape[1] != req_dimension
                     ):
                         node = graph.nodes[predecessor]
-                        node["filters"] = req_shape*2
-                        node["kernel_size"] = (kernel_size, kernel_size)
+                        if kernel_size >= 1:
+                            node["kernel_size"] = (kernel_size, kernel_size)
+                        else:
+                            node["kernel_size"] = (1, 1)
+                        print(req_shape, nodes[predecessor].shape[-1])
+                        if nodes[predecessor].shape[-1] != req_shape:
+                            print(f"Changing filters from {nodes[predecessor].shape[-1]} to {req_shape}")
+                            node['layer_type'] = 'Convolution'
+                            node["filters"] = req_shape
+                        print(node)
                         layer = load_layer(node)(nodes[predecessor])
                         normalized = BatchNormalization()(layer)
                         activation = Activation(
                             node["activation"]
                         )(normalized)
-                        drop = Dropout(0.9)(activation)
+                        drop = Dropout(0.2)(activation)
                         nodes[predecessor] = drop
-                concat = Add()([nodes[x] for x in predecessors])
+                    node_list.append(drop)
+                    print(graph.nodes[predecessor],predecessor)
+                    print(node_list)
+                concat = Add()(node_list)
             else:
                 concat = nodes[predecessors[0]]
-            nodes[node_] = concat
-        print('predecessors_built')
+            print(concat)
+            node_d = graph.nodes[node_]
+            node_d["filters"] = concat.shape[-1]*2
+            if concat.shape[1] < graph.nodes[node_]["kernel_size"][0]:
+                node_d["kernel_size"] = (1, 1)
+            node = load_layer(node_d)(concat)
+            normalized = BatchNormalization()(node)
+            activation = Activation(graph.nodes[node_]["activation"])(
+                normalized
+            )
+            drop = Dropout(0.2)(activation)
+            nodes[node_] = drop
+            print(concat)
         dropout_prob = random.uniform(0.2, 1)
         nodes[node_] = Dropout(dropout_prob)(nodes[node_])
     node_s = [nodes[node] for node in graph.nodes() if graph.out_degree(node) == 0]
-    print(node_s)
     if use_mean:
-        req_shape = max([x.shape[-1] for x in node_s])
-        req_dimension = max([x.shape[1] for x in node_s])
+        req_shape = max([x.shape[-1] for x in node_s])*2
+        req_dimension = min([x.shape[1] for x in node_s])
     else:
-        req_shape = max([x.shape[-1] for x in node_s])
-        req_dimension = max([x.shape[1] for x in node_s])
+        req_shape = max([x.shape[-1] for x in node_s])*2
+        req_dimension = min([x.shape[1] for x in node_s])
     for node, _ in enumerate(node_s):
         kernel_size = node_s[node].shape[1] - req_dimension + 1
         if (
@@ -168,23 +184,21 @@ def create_model(
             or node_s[node].shape[1] != req_dimension or kernel_size >= 1
         ):
             nodet = graph.nodes[node]
-            nodet["filters"] = req_shape*2
+            nodet["filters"] = req_shape
             nodet["kernel_size"] = (kernel_size, kernel_size)
-            print(kernel_size)
             layer = load_layer(nodet)(node_s[node])
             normalized = BatchNormalization()(layer)
             activation = Activation(graph.nodes[node]["activation"])(
                 normalized
             )
-            drop = Dropout(0.9)(activation)
+            drop = Dropout(0.2)(activation)
             conc = Concatenate()([activation, drop])
             nodes[node_] = conc
     if include_aux:
-        last_node = list(nodes.keys())[-1]
-        aux = aux_layer(nodes[last_node], num_classes)
-        model = keras.models.Model(inputs=input_layer, outputs=aux)
+        all_aux = [aux_layer(i,num_classes) for i in node_s]
+        model = keras.models.Model(inputs=input_layer, outputs=all_aux)
     else:
-        model = keras.models.Model(inputs=input_layer, outputs=nodes[-1])
+        model = keras.models.Model(inputs=input_layer, outputs=aux_layer(nodes[-1],num_classes))
     return model
 
 
